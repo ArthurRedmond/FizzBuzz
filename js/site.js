5d815fed87a88f649a464a8cb37e0df7ca9a34b2
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-09-23 18:19:32 -0400

    Updated layout

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-//Call HelloWorld 
-function HelloWorld() {
-    alert("Hello World");
+function getValues() {
+    let fizzNumber = document.getElementById("fizzNumber").value;
+    let buzzNumber = document.getElementById("buzzNumber").value;
+
+    fizzNumber = parseInt(fizzNumber);
+    buzzNumber = parseInt(buzzNumber);
 }
+
+function generateNumbers() {
+
+}
+
+function displayResults() {
+
+}
+// From Hundreds
+// function displayNumbers(numbers) {
+//     let templateRows = "";
+//     let className = "";
+//     for (let i = 0; i < numbers.length; i++) {
+//         let number = numbers[i];
+//         if (number % 2 == 0) {
+//             className = "even";
+//         } else {
+//             className = "odd";
+//         }
+//         templateRows += `<tr><td class="${className}">${number}</td></tr>`
+//     }
+//     document.getElementById("results").innerHTML = templateRows;
+// }
