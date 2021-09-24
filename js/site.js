83461ccff4f8920986b6a4a51d76e569058fc7e5
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-09-23 22:11:45 -0400

    Updated JS

--- js/site.js
+++ js/site.js
@@ -4,14 +4,36 @@ function getValues() {
 
     fizzNumber = parseInt(fizzNumber);
     buzzNumber = parseInt(buzzNumber);
+
+    let numbers = generateNumbers(fizzNumber, buzzNumber);
+    displayResults(numbers);
 }
 
-function generateNumbers() {
+function generateNumbers(fizzNumber, buzzNumber) {
+    let numbers = [];
 
+    for (let i = 1; i <= 100; i++) {
+        if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+            numbers.push("FIZZBUZZ");
+        } else if (i % fizzNumber == 0) {
+            numbers.push("FIZZ");
+        } else if (i % buzzNumber == 0) {
+            numbers.push("BUZZ");
+        } else {
+            numbers.push(i);
+        }
+    }
+    return numbers;
 }
 
-function displayResults() {
+function displayResults(numbers) {
+    let templateRows = "";
 
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        templateRows += `<tr><td>${number}</td></tr>`
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
 // From Hundreds
 // function displayNumbers(numbers) {
