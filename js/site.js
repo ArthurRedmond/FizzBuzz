c000a5810a84b0e2b82b0ed01bf2aba2bc93ff12
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-09-28 11:01:30 -0400

    Update js logic

--- js/site.js
+++ js/site.js
@@ -17,15 +17,8 @@ function fizzBuzz(fizzValue, buzzValue) {
     let returnArray = [];
 
     for (let i = 1; i <= 100; i++) {
-        if (i % fizzValue == 0 && i % buzzValue == 0) {
-            returnArray.push("FizzBuzz");
-        } else if (i % fizzValue == 0) {
-            returnArray.push("Fizz");
-        } else if (i % buzzValue == 0) {
-            returnArray.push("Buzz");
-        } else {
-            returnArray.push(i);
-        }
+        let value = ((i % fizzValue == 0 ? 'Fizz' : '') + (i % buzzValue == 0 ? 'Buzz' : '') || i);
+        returnArray.push(value);
     }
     return returnArray;
 }
@@ -45,14 +38,23 @@ function displayResults(fbArray) {
         let tableRow = document.importNode(templateRow.content, true);
 
         let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index + 1]);
         rowCols[1].textContent = fbArray[index + 1];
+
+        rowCols[2].classList.add(fbArray[index + 2]);
         rowCols[2].textContent = fbArray[index + 2];
+
+        rowCols[3].classList.add(fbArray[index + 3]);
         rowCols[3].textContent = fbArray[index + 3];
+
+        rowCols[4].classList.add(fbArray[index + 4]);
         rowCols[4].textContent = fbArray[index + 4];
 
         tableBody.appendChild(tableRow);
-
     }
 
 }
